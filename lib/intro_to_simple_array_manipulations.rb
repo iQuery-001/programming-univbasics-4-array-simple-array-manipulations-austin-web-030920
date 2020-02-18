3f987716a72285f5cfe1d6614fff607da30c598f
Dakota Willingham <kota@vaga.tk>
2020-02-18 21:54:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,5 +2,12 @@ def using_push(array, string)
   array.push(string)
 end
 
+def using_unshift(array, string)
+  array.unshift(element)
+end
+
 def using_pop(array)
   array.pop
+end
+
+def pop_with_args(
